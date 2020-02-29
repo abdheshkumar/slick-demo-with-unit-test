7e6f20063a82f61ae3fa0db5e60467cda66cab6c
Abdhesh Kumar <abdheshkumar90.rkg@gmail.com>
2020-02-29 10:50:32 +0000

    Move function into DAO

--- src/main/scala/abtechsoft/DAL.scala
+++ src/main/scala/abtechsoft/DAL.scala
@@ -2,4 +2,10 @@ package abtechsoft
 
 import slick.jdbc.JdbcProfile
 
-class DAL(val profile: JdbcProfile) extends Schema with DBProfile {}
+class DAL(val profile: JdbcProfile) extends Schema with DBProfile {
+  import profile.api._
+  def insert(s: Supplier*): DBIO[Option[Int]] = {
+    (suppliers ++= s) //.map(id => s.copy(id = Some(id)))
+  }
+  def getAll: DBIO[Seq[Supplier]] = suppliers.result
+}
--- src/main/scala/abtechsoft/Suppliers.scala
+++ src/main/scala/abtechsoft/Suppliers.scala
@@ -1,11 +1,7 @@
 package abtechsoft
 
-import slick.dbio.Effect
 import slick.jdbc.JdbcProfile
 import slick.lifted.ProvenShape
-import slick.sql.FixedSqlAction
-
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by abdhesh on 24/04/17.
@@ -29,10 +25,6 @@ trait Schema {
   }
 
   val suppliers = TableQuery[Suppliers]
-  def insert(s: Supplier*): DBIO[Option[Int]] = {
-    (suppliers ++= s) //.map(id => s.copy(id = Some(id)))
-  }
-  def getAll: DBIO[Seq[Supplier]] = suppliers.result
 }
 
 case class Supplier(id: Option[Int] = None, name: String)
